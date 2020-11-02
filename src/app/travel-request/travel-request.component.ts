b6b8978b6cd1d4e9494813edc2a61e3bb9f9eab3
erichaerr@outlook.com <erichaerr@outlook.com>
2020-11-02 13:29:09 -0600

    moved the array of destinations into an ng service

--- src/app/travel-request/travel-request.component.ts
+++ src/app/travel-request/travel-request.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IDestination } from '../models/destination';
 import { Itinerary } from '../models/itinerary';
-//import { Router } from '@angular/router';
+import { DestinationsService } from '../services/destinations.service';
 
 @Component({
   selector: 'app-travel-request',
@@ -14,58 +14,23 @@ import { Itinerary } from '../models/itinerary';
   min: number = 1;
   max: number = 10;
 
-  //destinations: IDestination[] = []
-  destinations: IDestination[] = [
-    {
-      "Id": 1,
-      "City": "Miami",
-      "AirportName" : "Miami International",
-      "AirportCode": "MIA",
-      "Description" : "Warm all year round",
-      "Rating": 4,
-      "RatingIcons": this.populateRatings(4),
-      "RatingType": "thermometer",
-      "imageUrl" : "assets/images/miami.png"
-    },
-    {
-      "Id" : 2,
-      "City" : "Chicago",
-      "AirportName" : "O'hare International",
-      "AirportCode" : "ORD",
-      "Description" : "Cold, cold, cold",
-      "Rating": 2,
-      "RatingIcons": this.populateRatings(2),
-      "RatingType": "snowflake",
-      "imageUrl" : "assets/images/chicago.png"
-    },
-    {
-      "Id" : 3,
-      "City" : "Denver",
-      "AirportName" : "Denver International",
-      "AirportCode" : "DIA",
-      "Description" : "Mountains and beer",
-      "Rating": 5,
-      "RatingIcons": this.populateRatings(5),
-      "RatingType": "snowflake",
-      "imageUrl" : "assets/images/denver.png"
-    }
-  ];
+  destinations: IDestination[];
 
 
   model = new Itinerary(
     "", new Date(), new Date(), "", "", 0
   )
 
-  constructor() { }
+  constructor(private destinationService: DestinationsService) { 
+    this.destinations = this.destinationService.getDestinations();
+  }
 
   ngOnInit(): void {
     console.log('onInit');
     console.log(this.destinations);
   }
 
-  populateRatings(num): number[]{
-    return new Array(num).fill(num)
-  }
+
 
   onSubmit(form): void {
     
