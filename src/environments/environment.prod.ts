3749edd58017b94287806c9fa4eaaebc9364f388
erichaerr@outlook.com <erichaerr@outlook.com>
2020-11-02 12:59:48 -0600

    forgot to update the prod version of environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  primaryOS: "Windows 10 WSL2 Ubuntu",
+  secondaryOS: "Windows 10",
+  ide: "Visual Studio Code - v.1.50.1"
 };
